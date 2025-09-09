8d0b97cbf99d2a6e3ad8bbd5b01058631e1e015e
Mustafa Binalhag <mbin828@gmail.com>
2025-09-09 17:34:58 +0300

    well known route

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,11 +1,23 @@
 package server
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
 
 func registerRoutes(e *echo.Echo, db *gorm.DB) {
+	e.GET("/.well-known/apple-developer-merchantid-domain-association", func(c echo.Context) error {
+		path := "public/.well-known/apple-developer-merchantid-domain-association"
+		b, err := os.ReadFile(path)
+		if err != nil {
+			return c.NoContent(http.StatusNotFound)
+		}
+		return c.Blob(http.StatusOK, "text/plain; charset=utf-8", b)
+	})
+
 	e.POST("/api/payment-pages", func(c echo.Context) error { return handleCreatePaymentPage(c, db) })
 	e.GET("/p/:merchant_id/:page_uid", func(c echo.Context) error { return handleViewPaymentPage(c, db) })
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,12 +2,25 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
 
 func Router(db *gorm.DB) *echo.Echo {
 	e := echo.New()
 	e.HideBanner = true
+	e.Logger.SetLevel(log.INFO)
+
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.RequestIDWithConfig(middleware.RequestIDConfig{
+		Generator: func() string { return newUUID() },
+	}))
+	e.Use(middleware.Recover())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `${time_rfc3339} id=${id} remote_ip=${remote_ip} method=${method} uri=${uri} status=${status} latency=${latency_human} bytes_in=${bytes_in} bytes_out=${bytes_out} ua=${user_agent} error=${error}\n`,
+	}))
+
 	e.Renderer = NewRenderer()
 
 	registerRoutes(e, db)
