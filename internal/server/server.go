f3b983e79b78462c5a49b66d1b4304e7bd343b9e
Mohamed Muflahi <muflahimohamed88@gmail.com>
2025-09-16 15:23:37 -0400

    test cors

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 func Router(db *gorm.DB) *echo.Echo {
@@ -16,7 +17,34 @@ func Router(db *gorm.DB) *echo.Echo {
 	e.Use(middleware.RequestIDWithConfig(middleware.RequestIDConfig{
 		Generator: func() string { return newUUID() },
 	}))
-	
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+    AllowOrigins: []string{"*"}, // allow any origin (development only!)
+    AllowMethods: []string{
+        http.MethodGet,
+        http.MethodHead,
+        http.MethodPut,
+        http.MethodPost,
+        http.MethodPatch,
+        http.MethodDelete,
+        http.MethodOptions,
+    },
+    AllowHeaders: []string{
+        echo.HeaderOrigin,
+        echo.HeaderContentType,
+        echo.HeaderAccept,
+        echo.HeaderAuthorization,
+        "X-Requested-With",
+        "X-CSRF-Token",
+    },
+    ExposeHeaders: []string{
+        echo.HeaderContentLength,
+        echo.HeaderContentType,
+        echo.HeaderAuthorization,
+    },
+    AllowCredentials: false, // must stay false with "*" origins
+    MaxAge: 86400,
+}))
+
 	e.Use(middleware.Recover())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `${time_rfc3339} id=${id} remote_ip=${remote_ip} method=${method} uri=${uri} status=${status} latency=${latency_human} bytes_in=${bytes_in} bytes_out=${bytes_out} ua=${user_agent} error=${error}\n`,
